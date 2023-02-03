8a52df44a653d3ae46279a286a1c303c7f7378f5
Your Name <Solomogokid@gmail.com>
2023-02-02 19:24:31 -0800

    Task 6 done

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""   """
 
 from typing import Union
 
 def sum_mixed_list(input_list:list[Union[float, int]]) -> float:
+    """   """
     sum: float = 0
     for i in range(len(input_list)):
         sum += input_list[i]
-    return sum
+    return (sum)
