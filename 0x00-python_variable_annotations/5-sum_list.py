f752b8858665bcb0e212b33f923eee7ef3a21fd7
Your Name <Solomogokid@gmail.com>
2023-02-03 04:40:50 -0800

    Task 5 sum list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,9 +3,10 @@
 
 from typing import List
 
-def sum_list(input_list:List[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """ """
     sum: float = 0
     for i in range(len(input_list)):
         sum += input_list[i]
-    return (sum_
+    return (sum)
