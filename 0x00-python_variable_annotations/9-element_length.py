47310e73573062b98bd52649b73096f153cb4c59
Your Name <Solomogokid@gmail.com>
2023-02-03 07:01:54 -0800

    Task 9 element length

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""  """
+""" Return a Tuple and Sequence of int """
 
-from typing  import Sequence, Tuple, Iterator, List
+from typing import Sequence, Tuple, Iterator, List
+
 
 def element_length(lst: Iterator[Sequence]) -> List[Tuple[Sequence, int]]:
-    """  """
+    """ Function tha Iterate through the sequence """
     return ([(i, len(i)) for i in lst])
