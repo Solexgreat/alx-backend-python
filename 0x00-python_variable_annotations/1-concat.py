08bc95b3bf0e9cc8f66b4853ec4a0518038cdfeb
Your Name <Solomogokid@gmail.com>
2023-02-03 04:56:45 -0800

    Task 1 concat\

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-"""   """
+""" Return the concat of string """
+
 
 def concat(str1: str, str2: str) -> str:
-    """    """
+    """ function to add and Return str """
     return (str1 + str2)
