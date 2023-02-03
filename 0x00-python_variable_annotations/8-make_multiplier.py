dc451c8d599ae330efda90dc919ab20d2bb88248
Your Name <Solomogokid@gmail.com>
2023-02-02 19:27:22 -0800

    Task 8 return callable

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+""" """
 
 from typing import Callable
 
 def make_multiplier(multiplier: float)-> Callable[[float], float]:
+    """ """
     def mul(multiplier: float) -> float:
+        """  """
         return multiplier * multiplier
 
-    return mul
+    return (mul)
