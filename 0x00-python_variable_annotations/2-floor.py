7f503f0b980f98c0c37c7c9252f4bcd67b5bcf6d
Your Name <Solomogokid@gmail.com>
2023-02-03 05:03:35 -0800

    Task 2 Take float give int

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-""" """
+""" Return an int """
 
-def floor(n:float) -> int:
-    """ """
+
+def floor(n: float) -> int:
+    """ function that takes float and return int """
     return (int(n))
