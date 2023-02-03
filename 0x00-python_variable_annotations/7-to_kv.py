6a77820b0d68001716f4ac9c35678edace3b5a7f
Your Name <Solomogokid@gmail.com>
2023-02-02 19:42:30 -0800

    Task 7 done

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,8 @@
 
 from typing import Tuple, Union
 
-def to_kv(k:str, v: Union[int, float]) -> Tuple:
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """   """
     tup: Tuple[str, float] = [k, v*v]
     return (tup)
