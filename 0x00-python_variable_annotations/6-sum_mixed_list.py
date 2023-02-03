2f381336c3627d14fb50c518ebc077f662274efb
Your Name <Solomogokid@gmail.com>
2023-02-03 05:54:40 -0800

    Task 6 sum mixed listed

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""   """
+""" Return the sum of mixed List """
 
 from typing import Union, List
 
 
 def sum_mixed_list(input_list: List[Union[int, float]]) -> float:
-    """   """
+    """ function takes list of different type and return the sum """
     sum: float = 0
     for i in range(len(input_list)):
         sum += input_list[i]
