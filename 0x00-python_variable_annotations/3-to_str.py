78926c95c4997571f0395ecfa8060b35e2c6bce0
Your Name <Solomogokid@gmail.com>
2023-02-03 05:06:33 -0800

    Task 3, take a float and return str

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""  """
-def to_str(n:float) -> str:
-    """  """
+""" Returns String """
+
+
+def to_str(n: float) -> str:
+    """ function takes a float abd return a string """
     return (str(n))
