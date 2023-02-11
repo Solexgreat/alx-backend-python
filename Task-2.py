33f88c77de96db3d727b8a51bd7c97b144ff0254
Solexgreat <solomonGodkid@gmail.com>
2023-02-12 00:25:39 +0100

    Task 1 done Understood

--- Task-2.py
+++ Task-2.py
@@ -9,5 +9,5 @@ async def wait_n(n: int, max_delay: int )-> List[float]:
                         for i in range(n) ]
     completed_task = []
     for task in asyncio.as_completed(create):
-        completed_task.append(await i)
-    return completed_task    
+        completed_task.append(await task)
+    return (completed_task)   
